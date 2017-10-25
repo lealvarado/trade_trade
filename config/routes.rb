2a7938020f26f10547fe7f5b3b2662c519c16fea
Leslie Alvarado <lesliemalvarado@gmail.com>
2017-10-25 10:53:25 -0400

    SignIn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'sign_up' => 'devise/registrations#new'
+
   devise_for :users
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
