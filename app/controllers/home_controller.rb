889f0fdbea994c62f9a90d5d23ba2904710fd71d
Leslie Alvarado <lesliemalvarado@gmail.com>
2017-10-26 10:52:12 -0400

    checkboxes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
 	
   def index
+  	@skills = Skill.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :skills
   root 'home#index'
 
   get 'sign_up' => 'devise/registrations#new'
